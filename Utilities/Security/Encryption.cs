864278632ce62c5aacf51f82d8981e3be30b262f
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-09-05 00:02:44 -0500

    feat: implement encryption and response messaging in Utilities library

---
+++ Utilities/Security/Encryption.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Utilities.Security
+{
+    public class Encryption
+    {
+        public static string EncriptarSHA256(string input)
+        {
+            using SHA256 sha256 = SHA256.Create();
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder resultado = new();
+            foreach (byte b in bytes)
+            {
+                resultado.Append(b.ToString("x2"));
+            }
+            return resultado.ToString();
+        }
+    }
+}
+
+//Scaffold-DbContext "Server=DESKTOP-8CQM9OG\SQLSEXPRESS;Database=SISTEMA_CINE;Trusted_Connection=True;TrustServerCertificate=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -Context DBContext -f
---
+++ Utilities/Shared/Pagination.cs
@@ -0,0 +1,10 @@
+﻿namespace Utilities.Shared
+{
+    public class Pagination<P>
+    {
+        public List<P> Items { get; set; } = new();
+        public int TotalCount { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
